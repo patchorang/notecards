962152a5ff2c7d082f510283adf79c4aef6d101c
Adam Menz <mz.adamQ@gmail.com>
2023-02-08 17:22:25 -0500

    serverclient

--- App.js
+++ App.js
@@ -11,6 +11,13 @@ mongoose.connect(process.env.MONGODB_URI);
 app.use(cors());
 app.use(express.json());
 
+// const path = require("path");
+const path = __dirname + "/client/public";
+app.use(express.static(path));
+app.get("/", function (req, res) {
+  res.sendFile(path + "index.html");
+});
+// app.use(express.static(path.join(__dirname, "/client/public")));
 app.use("/api/decks", deckRouter);
 app.use("/api/flashcards", flashcardRouter);
 
